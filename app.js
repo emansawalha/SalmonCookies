baa77ea301162abd537e5a6103593504ef4e8843
emansawalha <emansawalha10@gmail.com>
2021-02-14 14:54:34 +0200

    first update

---
+++ app.js
@@ -0,0 +1,21 @@
+'use strict'; 
+Seattle {
+
+minCustomr:   ;
+maxCustomer:  ;
+avgCookies:   ;
+
+this. = randomValue(3,12) + ' months';
+
+};
+
+function randomValue(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
+
+
+
+
+
+
